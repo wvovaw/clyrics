b4795722d97839d75833074bff4be1407848189e
wvovaw <vladimir5313375015@gmail.com>
2020-07-01 05:51:36 +0500

    Change Genius to Musixmatch. Addartist search

--- logic.js
+++ logic.js
@@ -1,25 +1,35 @@
 const request = require('request');
 const cherio = require('cheerio');
 
-const getLyrics = async (artist, title) => {
-  let url = 'https://genius.com/'
-    + artist.toLowerCase().trim().replace(/ /g, '-')
-    + '-'
-    + title.toLowerCase().trim().replace(/ /g, '-')+ '-lyrics';
+// Returns lyrics for a concrete song
+const getLyricsMusixmatch = async (artist, title) => {
+  let url = 'https://musixmatch.com/lyrics/'
+    + encodeURI(artist.toLowerCase().trim().replace(/ /g, '-'))
+    + '/'
+    + encodeURI(title.toLowerCase().trim().replace(/ /g, '-'));
 
   let songPage = await new Promise((resolve, reject) => {
     request(url, (error, response, html) => {
       if(!error && response.statusCode == 200){
         const $ = cherio.load(html);
-        const songBodyLyrics = $('.lyrics').text();
+        let spans;
+        if($('.lyrics__content__warning')[0]) {
+          console.log('The lyrics are waiting for review!');
+          spans = $('.lyrics__content__warning');
+        } 
+        else {
+          spans = $('.lyrics__content__ok');
+        } 
+        let copyright = $('.mxm-lyrics__copyright');
+        const songBodyLyrics = spans.text() + '\n\n\n' + copyright.text();
         resolve(songBodyLyrics);
       }     
       else reject(error);
     });
   }).catch(() => {
-    console.log('Song page don\'t exists. Perform searching ...');
+    console.log('Song page doesn\'t exist. Performing a search ...');
   });
-  
+
   if(songPage != undefined) {
     let lyrics = songPage.replace(/<\/?[^>]+(>|$)/g, '');
     lyrics = lyrics.replace(/&apos;/g, '\'');
@@ -27,22 +37,31 @@ const getLyrics = async (artist, title) => {
     return(lyrics);
   }
   else return(null);
+
 };
 
-const searchSong = async (artist='', title='') => {
-  let url = 'https://genius.com/search?q=';
-  if(artist != '' && title != '') {
+// Returns list of the relevant songs
+const searchSongMusixmatch = async (artist='', title='') => {
+  let url = 'https://www.musixmatch.com/';
+  
+  if(artist != '' && title != '') { // Common search 'artist - title'
     url = url 
-      + artist.toLowerCase().trim().replace(/ /g, '%20') + '%20'
-      + title.toLowerCase().trim().replace(/ /g, '%20');
+      + 'search/'
+      + encodeURI(artist.toLowerCase().trim().replace(/ /g, '%20') + '%20')
+      + encodeURI(title.toLowerCase().trim().replace(/ /g, '%20')) + '/tracks';
+    console.log(url);
   }
-  else if(artist == '') {
+  else if(artist == '') { // Search by song title
     url = url
-      + title.toLowerCase().trim().replace(/ /g, '%20');
+      + 'search/'
+      + encodeURI(title.toLowerCase().trim().replace(/ /g, '%20')) + '/tracks';
+    console.log(url);
   }
-  else if(title == '') {
+  else if(title == '') { // Find top artists songs
     url = url
-      + artist.toLowerCase().trim().replace(/ /g, '%20');
+      + 'artist/' 
+      + encodeURI(artist.toLowerCase().trim().replace(/ /g, '-'));
+    console.log(url);
   }
   else return(null);
 
@@ -50,25 +69,24 @@ const searchSong = async (artist='', title='') => {
     request(url, (error, response, html) => {
       if(!error && response.statusCode == 200){
         const $ = cherio.load(html);
-        console.log($.text());
         let results = [];
-        $('.mini_card-title_and_subtitle').each((i, el) => {
-          const artist = $(el).find('.mini_card-subtitle').text();
-          const title = $(el).find('.mini_card-subtitle').text();
-          results += '[' + i + ']: ' + artist + ' - ' + title;
+        $('.track-card').each((i, el) => {
+          const artist = $(el).find('.title').text();
+          const title = $(el).find('.artist').text();
+          results.push(artist + ' - ' + title);
         });
         resolve(results);
       }     
       else reject(error);
     });
   }).catch(() => {
-    console.log('None was found.');
+    console.log('searchSongMusixmatch: Nothing was found.');
   });
 
-  if(searchResult != undefined) {
+  if(searchResult != undefined && searchResult.length > 0) {
     return searchResult;
   }
   else return(null);
 };
 
-module.exports = {getLyrics, searchSong};
+module.exports = { getLyricsMusixmatch, searchSongMusixmatch };
